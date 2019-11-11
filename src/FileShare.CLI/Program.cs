e1ffb98ef35690e1b9d5c01f5a7bb53a8e96db79
Ryan Condron <rebelwebdevelopment@gmail.com>
2019-11-10 22:34:04 -0600

    Aded Start of Console Application

---
+++ src/FileShare.CLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FileShare.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Displaying File Contents In the Share");
+        }
+    }
+}
